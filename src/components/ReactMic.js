a6a3984255da5605c5dea770302e0fa3c7dd3ca1
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-09-21 18:30:34 -0700

    remove unused vars

--- src/components/ReactMic.js
+++ src/components/ReactMic.js
@@ -4,84 +4,53 @@
 // distortion curve for the waveshaper, thanks to Kevin Ennis
 // http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
 
-import React, { Component }   from 'react'
-import { string, number, bool, func } from 'prop-types';
-import { MicrophoneRecorder } from '../libs/MicrophoneRecorder';
-import AudioContext           from '../libs/AudioContext';
-import AudioPlayer            from '../libs/AudioPlayer';
+import React, { Component } from "react";
+import MicrophoneRecorder from "../libs/MicrophoneRecorder";
+import AudioPlayer from "../libs/AudioPlayer";
 
 export default class ReactMic extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      microphoneRecorder  : null,
-    }
+      microphoneRecorder: null
+    };
   }
 
   componentDidMount() {
-    const {
-      onSave,
-      onStop,
-      onStart,
-      onData,
-      audioElem,
+    const { onSave, onStop, onStart, onData, audioElem, audioBitsPerSecond, mimeType } = this.props;
+    const options = {
       audioBitsPerSecond,
       mimeType
-    } = this.props;
-    const options = {
-      audioBitsPerSecond : audioBitsPerSecond,
-      mimeType           : mimeType
-    }
+    };
 
-    if(audioElem) {
+    if (audioElem) {
       AudioPlayer.create(audioElem);
     } else {
       this.setState({
-        microphoneRecorder  : new MicrophoneRecorder(
-                                onStart,
-                                onStop,
-                                onSave,
-                                onData,
-                                options
-                              ),
+        microphoneRecorder: new MicrophoneRecorder(onStart, onStop, onSave, onData, options)
       });
     }
-
   }
 
   render() {
-    const { record, onStop, width, height, children } = this.props;
+    const { record, onStop, children } = this.props;
     const { microphoneRecorder } = this.state;
 
-    if(record) {
-      if(microphoneRecorder) {
+    if (microphoneRecorder) {
+      if (record) {
         microphoneRecorder.startRecording();
-      }
-    } else {
-      if (microphoneRecorder) {
+      } else {
         microphoneRecorder.stopRecording(onStop);
       }
     }
 
-    return (
-      <React.Fragment>
-        {children}
-      </React.Fragment>)
+    return <React.Fragment>{children}</React.Fragment>;
   }
 }
 
-ReactMic.propTypes = {
-  className       : string,
-  audioBitsPerSecond: number,
-  mimeType        : string,
-  record          : bool.isRequired,
-  onStop          : func,
-  onData          : func
-};
-
 ReactMic.defaultProps = {
-  className         : 'record',
+  className: "record",
   audioBitsPerSecond: 128000,
-  mimeType          : 'audio/webm;codecs=opus',
-  record            : false
-}
+  mimeType: "audio/webm;codecs=opus",
+  record: false
+};
--- src/libs/MicrophoneRecorder.js
+++ src/libs/MicrophoneRecorder.js
@@ -1,14 +1,9 @@
-import AudioContext from './AudioContext';
-import MediaRecorder from 'audio-recorder-polyfill';
+import MediaRecorder from "audio-recorder-polyfill";
 
-let analyser;
-let audioCtx;
 let mediaRecorder;
 let chunks = [];
 let startTime;
-let stream;
 let mediaOptions;
-let blobObject;
 let onStartCallback;
 let onStopCallback;
 let onSaveCallback;
@@ -16,70 +11,69 @@ let onDataCallback;
 
 const constraints = { audio: true };
 
-navigator.getUserMedia = (navigator.getUserMedia ||
-                          navigator.webkitGetUserMedia ||
-                          navigator.mozGetUserMedia ||
-                          navigator.msGetUserMedia);
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
 
-export class MicrophoneRecorder {
+export default class MicrophoneRecorder {
   constructor(onStart, onStop, onSave, onData, options) {
-    onStartCallback= onStart;
-    onStopCallback= onStop;
+    onStartCallback = onStart;
+    onStopCallback = onStop;
     onSaveCallback = onSave;
     onDataCallback = onData;
-    mediaOptions= options;
+    mediaOptions = options;
   }
 
   startRecording = () => {
     startTime = Date.now();
-    analyser = null;
-    audioCtx = null;
     chunks = [];
-    stream = null;
-    blobObject = null;
     mediaRecorder = null;
 
     if (navigator.mediaDevices) {
-      navigator.mediaDevices.getUserMedia(constraints)
-        .then((str) => {
-          stream = str;
-          mediaRecorder = new MediaRecorder(str);
-          if(onStartCallback) { onStartCallback() };
-          mediaRecorder.addEventListener('dataavailable', e => {
-            chunks = e.data;
-            if(onDataCallback) {
-              onDataCallback(e.data);
-            }
-          })
-
-          mediaRecorder.start();
-          mediaRecorder.addEventListener('stop', this.onStop)
+      navigator.mediaDevices.getUserMedia(constraints).then(str => {
+        mediaRecorder = new MediaRecorder(str);
+        if (onStartCallback) {
+          onStartCallback();
+        }
+        mediaRecorder.addEventListener("dataavailable", e => {
+          chunks = e.data;
+          if (onDataCallback) {
+            onDataCallback(e.data);
+          }
         });
 
+        mediaRecorder.start();
+        mediaRecorder.addEventListener("stop", this.onStop);
+      });
     } else {
-      alert('Your browser does not support audio recording');
+      alert("Your browser does not support audio recording");
     }
-  }
+  };
 
-  stopRecording () {
+  stopRecording = () => {
     if (mediaRecorder) {
       mediaRecorder.stop();
-      mediaRecorder.stream.getTracks()[0].stop()
-      mediaRecorder.stream.getTracks().forEach(i => i.stop())
+      mediaRecorder.stream.getTracks()[0].stop();
+      mediaRecorder.stream.getTracks().forEach(i => i.stop());
     }
-  }
+  };
 
-  onStop() {
-    const blobObject =  {
-      blob      : chunks,
-      startTime : startTime,
-      stopTime  : window.Date.now(),
-      options   : mediaOptions,
-      blobURL   : window.URL.createObjectURL(chunks)
+  onStop = () => {
+    const blobObject = {
+      blob: chunks,
+      startTime,
+      stopTime: window.Date.now(),
+      options: mediaOptions,
+      blobURL: window.URL.createObjectURL(chunks)
+    };
+
+    if (onStopCallback) {
+      onStopCallback(blobObject);
     }
-
-    if(onStopCallback) { onStopCallback(blobObject) };
-    if(onSaveCallback) { onSaveCallback(blobObject) };
-  }
-
+    if (onSaveCallback) {
+      onSaveCallback(blobObject);
+    }
+  };
 }
