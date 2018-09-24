6eacabaef2ed75a0a3dbbd4438a2349143f7cffa
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-09-24 09:20:21 -0700

    postinstall

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React, {Component}          from 'react';
 import { render }                  from 'react-dom';
 
-import { ReactMic, saveRecording } from '../../src';
+import { ReactMic } from '../../src';
 
 require ('./styles.scss');
 
